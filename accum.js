488f53bd5a058f08d0a0809ee4da0696e382cddc
alaamigdady <alaamohammad283@gmail.com>
2018-02-27 09:20:36 +0200

    accum

---
+++ accum.js
@@ -0,0 +1,38 @@
+// This time no story, no theory. The examples below show you what do you need, write a function to accomplich that
+// Examples:
+// accum("abcd");    // "A-Bb-Ccc-Dddd"
+// accum("RqaEzty"); // "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+// accum("cwAt");    // "C-Ww-Aaa-Tttt"
+
+    function accum(input)
+    {
+        //your code is here
+     var wholeStr="";
+     var strArr=input.split("");
+       	for(var i=0; i<strArr.length;i++)
+       	{
+        	var result="";
+        	
+        	for(var j=0;j<=i;j++)
+        	{
+        		if(j===0)
+        		{
+        			result+=strArr[i].toUpperCase()
+        		}
+        			else
+        		{
+        			result +=strArr[i].toLowerCase()
+        		}
+        	}
+			if(i!==strArr.length-1)
+				{
+        			wholeStr +=result+"-";
+        		}
+			else
+				{ 
+				wholeStr +=result
+				}
+        }
+        wholeStr.slice(wholeStr.length-1);
+		return wholeStr;
+    }
